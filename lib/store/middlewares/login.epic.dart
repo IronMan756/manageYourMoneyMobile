527b2f0950b19bbcf299644156e0ddad40f0f8af
Andrew <andrewstarodubtsev@Andrews-iMac.local>
2020-06-18 20:14:30 +0300

    feat: fix auth epic(catch error) [ref: 16]

--- lib/store/middlewares/login.epic.dart
+++ lib/store/middlewares/login.epic.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:manageYourMoneyMobile/common/services/auth.service.dart';
-import 'package:manageYourMoneyMobile/screens/auth.dart';
 import 'package:manageYourMoneyMobile/screens/home.dart';
 import 'package:manageYourMoneyMobile/store/actions/auth.action.dart';
 import 'package:redux_epics/redux_epics.dart';
@@ -11,12 +10,11 @@ Stream<dynamic> loginEpic(Stream<dynamic> actions, EpicStore<dynamic> _store) {
   return actions.where((dynamic action) => action is LoginPending).switchMap(
       (dynamic action) =>
           Stream<dynamic>.fromFuture(logIn(action as LoginPending))
-              .expand((dynamic token) => <dynamic>[
+              .expand((dynamic data) => <dynamic>[
                     LoginSuccess(),
-                    PushReplacementAction(MaterialPageRoute<void>(
+                     if (data != null) PushReplacementAction(MaterialPageRoute<void>(
                         builder: (BuildContext context) =>
-                            const HomeScreen()))
-                  ]));
+                            const HomeScreen())) else null ]));
 }
 
 Stream<dynamic> signUpEpic(Stream<dynamic> actions, EpicStore<dynamic> _store) {
@@ -25,9 +23,5 @@ Stream<dynamic> signUpEpic(Stream<dynamic> actions, EpicStore<dynamic> _store) {
           Stream<dynamic>.fromFuture(signUp(action as SignUpPending))
               .expand((dynamic data) => <dynamic>[
                     SignUpSuccess(),
-                    data != null ??
-                    PushAction(MaterialPageRoute<void>(
-                        builder: (BuildContext context) =>
-                            const AuthorizationScreen()))
                   ]));
 }
