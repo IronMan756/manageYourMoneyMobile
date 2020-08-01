f91d9d1570e2f012865fd52fb0720b1c9afb54c7
Andrew <andrewstarodubtsev@Andrews-iMac.local>
2020-08-01 19:44:17 +0300

    Feat: make simple template for pocket item in list view [ref: 30]

--- lib/screens/incoms.dart
+++ lib/screens/incoms.dart
@@ -45,14 +45,124 @@ class _IncomsScreenState extends State<IncomsScreen> {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text(state.incomes[index].name.toString()),
-                              // Text(':'),
-                              // Text(state.incomes[index].balance.toString())
-                            ],
-                          )
+                          Container(
+                            height: 100,
+                            width: MediaQuery.of(context).size.width,
+                            child: FittedBox(
+                                child: Material(
+                                    elevation: 25.0,
+                                    borderRadius: BorderRadius.circular(5.0),
+                                    child: Row(
+                                      children: <Widget>[
+                                        Container(
+                                          height: 100,
+                                          // width: MediaQuery.of(context)
+                                          //         .size
+                                          //         .width *
+                                          //     0.27,
+                                          child: ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                              child: const Image(
+                                                  fit: BoxFit.contain,
+                                                  // alignment: Alignment.topLeft,
+                                                  image: NetworkImage(
+                                                      'https://www.birthdaywishes.expert/wp-content/uploads/2015/10/good-morning-image-sunflower.jpg'))),
+                                        ),
+                                        Container(
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.37,
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: <Widget>[
+                                                // Text('Income Name',
+                                                //     style: TextStyle(
+                                                //         fontSize: 20)),
+                                                // Text(
+                                                //     state.incomes[index]
+                                                //         .name
+                                                //         .toString(),
+                                                //     style: TextStyle(
+                                                //         fontSize: 20,
+                                                //         fontWeight:
+                                                //             FontWeight
+                                                //                 .bold)),
+                                                // const Padding(
+                                                //     padding:
+                                                //         EdgeInsets.all(
+                                                //             5),
+                                                //     child:
+                                                Padding(
+                                                  padding:
+                                                      const EdgeInsets.only(
+                                                          left: 8, top: 0),
+                                                  child: Text(
+                                                      state.incomes[index].name
+                                                          .toString(),
+                                                      style: TextStyle(
+                                                          fontSize: 17,
+                                                          fontWeight:
+                                                              FontWeight.bold)),
+                                                ),
+                                                const Padding(
+                                                    padding: EdgeInsets.all(5),
+                                                    child: Text(
+                                                        'Available: 400€')),
+                                                const Padding(
+                                                    padding: EdgeInsets.all(5),
+                                                    child: Text('adsouvh')),
+                                                // const Padding(
+                                                //     padding: EdgeInsets.all(5)),
+                                                // Text('Text3')),
+                                              ],
+                                            )),
+                                        Container(
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.37,
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: <Widget>[
+                                                // Padding(
+                                                //   padding:
+                                                //       const EdgeInsets.only(
+                                                //           left: 8, top: 0),
+                                                //   child:
+                                                Text(
+                                                  state.incomes[index].name
+                                                      .toString(),
+                                                ),
+
+                                                const Padding(
+                                                    padding: EdgeInsets.all(5),
+                                                    child: Text(
+                                                        'Available: 400€')),
+                                                const Padding(
+                                                    padding: EdgeInsets.all(5),
+                                                    child: Text('adsouvh')),
+                                                // const Padding(
+                                                //     padding: EdgeInsets.all(5)),
+                                                // Text('Text3')),
+                                              ],
+                                            )),
+                                      ],
+                                    ))),
+                          ),
+                          // ),
+
+                          // Text(state.incomes[index].balance.toString())
+                          // ],
+                          // ),
+                          SizedBox(
+                            height: 25,
+                          ),
+                          Text(state.incomes[index].name.toString()),
+                          // Text(':'),
                         ],
                       ));
                 }),
--- lib/screens/pouch.dart
+++ lib/screens/pouch.dart
@@ -35,29 +35,168 @@ class _PouchScreenState extends State<PouchScreen> {
         builder: (BuildContext context, _ViewModel state) {
           print(store.state.purses);
           return Scaffold(
-              body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListView.builder(
-                itemCount:
-                    state.purses != null ? state.purses.toList().length : 0,
-                itemBuilder: (BuildContext ctxt, int index) {
-                  return Container(
-                      margin: const EdgeInsets.all(10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+            body:
+                //   Padding(
+                // padding: const EdgeInsets.all(8.0),
+                // child:
+                ListView.builder(
+                    itemCount:
+                        state.purses != null ? state.purses.toList().length : 0,
+                    itemBuilder: (BuildContext ctxt, int index) {
+                      return Container(
+                          margin: const EdgeInsets.only(top: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
                             children: <Widget>[
-                              Text(state.purses[index].name.toString()),
-                              Text(':'),
-                              Text(state.purses[index].balance.toString())
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Container(
+                                    // height: 100,
+                                    width: MediaQuery.of(context).size.width *
+                                        0.95,
+                                    child: FittedBox(
+                                        child: Material(
+                                            elevation: 25.0,
+                                            borderRadius:
+                                                BorderRadius.circular(5.0),
+                                            child: Row(
+                                              children: <Widget>[
+                                                Container(
+                                                  height: 100,
+                                                  child: ClipRRect(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              5),
+                                                      child: const Image(
+                                                          // fit: BoxFit.contain,
+                                                          // alignment:
+                                                          //     Alignment.topLeft,
+                                                          image: NetworkImage(
+                                                              'https://www.birthdaywishes.expert/wp-content/uploads/2015/10/good-morning-image-sunflower.jpg'))),
+                                                ),
+                                                Container(
+                                                    width:
+                                                        MediaQuery.of(context)
+                                                                .size
+                                                                .width *
+                                                            0.5,
+                                                    child: Column(
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
+                                                      children: <Widget>[
+                                                        // Text('Income Name',
+                                                        //     style: TextStyle(
+                                                        //         fontSize: 20)),
+                                                        // Text(
+                                                        //     state.incomes[index]
+                                                        //         .name
+                                                        //         .toString(),
+                                                        //     style: TextStyle(
+                                                        //         fontSize: 20,
+                                                        //         fontWeight:
+                                                        //             FontWeight
+                                                        //                 .bold)),
+                                                        // const Padding(
+                                                        //     padding:
+                                                        //         EdgeInsets.all(
+                                                        //             5),
+                                                        //     child:
+                                                        Padding(
+                                                          padding:
+                                                              const EdgeInsets
+                                                                      .only(
+                                                                  left: 8,
+                                                                  top: 0),
+                                                          child: Text(
+                                                              state
+                                                                  .purses[index]
+                                                                  .name
+                                                                  .toString(),
+                                                              style: TextStyle(
+                                                                  fontSize: 17,
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .bold)),
+                                                        ),
+                                                        Padding(
+                                                            padding:
+                                                                const EdgeInsets
+                                                                    .all(5),
+                                                            child: Text(
+                                                                'Available: ${state.purses[index].balance.toString()} grn')),
+                                                        const Padding(
+                                                            padding:
+                                                                EdgeInsets.all(
+                                                                    5),
+                                                            child: Text(
+                                                                'adsouvh')),
+                                                      ],
+                                                    )),
+                                                Container(
+                                                    width:
+                                                        MediaQuery.of(context)
+                                                                .size
+                                                                .width *
+                                                            0.35,
+                                                    child: Column(
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
+                                                      children: <Widget>[
+                                                        Padding(
+                                                            padding:
+                                                                const EdgeInsets
+                                                                        .only(
+                                                                    left: 8,
+                                                                    top: 0),
+                                                            child:
+                                                                Row(children: <
+                                                                    Widget>[
+                                                              const Text(
+                                                                  'Bal:'),
+                                                              Text(
+                                                                state
+                                                                    .purses[
+                                                                        index]
+                                                                    .balance
+                                                                    .toString(),
+                                                                style: TextStyle(
+                                                                    fontWeight:
+                                                                        FontWeight
+                                                                            .w700,
+                                                                    fontSize:
+                                                                        16),
+                                                              ),
+                                                              const Text('grn',
+                                                                  style: TextStyle(
+                                                                      fontSize:
+                                                                          10)),
+                                                            ])),
+                                                        const Padding(
+                                                            padding:
+                                                                EdgeInsets.all(
+                                                                    5),
+                                                            child: Text(
+                                                                'Available: 400€')),
+                                                        const Padding(
+                                                            padding:
+                                                                EdgeInsets.all(
+                                                                    5),
+                                                            child: Text(
+                                                                'adsouvh')),
+                                                      ],
+                                                    )),
+                                              ],
+                                            ))),
+                                  ),
+                                ],
+                              )
                             ],
-                          )
-                        ],
-                      ));
-                }),
-          ));
+                          ));
+                    }),
+          );
         });
   }
 }
