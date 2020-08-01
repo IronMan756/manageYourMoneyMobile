7c2555f9a48973b6e02684ba3234c273eb5ef8f9
Andrew <andrewstarodubtsev@Andrews-iMac.local>
2020-08-01 15:53:22 +0300

    Feat: add all models, services for all entities [ref:22]

--- lib/common/services/transactions.service.dart
+++ lib/common/services/transactions.service.dart
@@ -1,28 +1,30 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:manageYourMoneyMobile/common/constants/config.dart';
 import 'package:manageYourMoneyMobile/common/services/toaster.service.dart';
-import 'package:manageYourMoneyMobile/store/models/expence.model.dart';
+import 'package:manageYourMoneyMobile/store/models/transaction.model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:http/http.dart' as http;
 
 // ignore: missing_return
-Future<List<ExpenceModel>> getExpences() async {
+Future<List<TransactionModel>> getTransactions() async {
   try {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     final String token = prefs.getString('access_token');
     final http.Response response =
-        await http.get('${getBaseApiURL()}expences', headers: {
+        await http.get('${getBaseApiURL()}transactions', headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
       'Authorization': 'Bearer $token',
     });
     if (response.statusCode == 200) {
+      print(response.body);
       return json
           .decode(response.body)['data']
-          .map<ExpenceModel>((dynamic item) =>
-              ExpenceModel.fromJson(item as Map<String, dynamic>))
-          .toList() as List<ExpenceModel>;
+          .map<TransactionModel>((dynamic item) =>
+              TransactionModel.fromJson(item as Map<String, dynamic>))
+          .toList() as List<TransactionModel>;
     }
   } catch (e) {
     print(e);
